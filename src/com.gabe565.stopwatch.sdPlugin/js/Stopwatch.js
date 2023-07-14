f35adf479ae233732e09493b6fbb150d23726344
Gabe Cook <gabe565@gmail.com>
2023-07-13 22:01:48 -0500

    :art: Organize Stopwatch functions

--- src/com.gabe565.stopwatch.sdPlugin/js/Stopwatch.js
+++ src/com.gabe565.stopwatch.sdPlugin/js/Stopwatch.js
@@ -12,84 +12,6 @@ class Stopwatch {
     })();
   }
 
-  start() {
-    console.log("Starting stopwatch");
-    this.state = States.Running;
-    this.startTime = new Date();
-    this.beginTick();
-  }
-
-  beginTick() {
-    this.tick();
-    this.stopTick();
-    this.tickInterval = setInterval(() => this.tick(), 1000);
-  }
-
-  stopTick() {
-    if (this.tickInterval !== null) {
-      clearInterval(this.tickInterval);
-    }
-  }
-
-  tick() {
-    if (this.frame) {
-      this.frame.querySelector("text").innerHTML = this.formattedTime;
-      const contents = new XMLSerializer().serializeToString(this.frame);
-      const encoded = `data:image/svg+xml;base64,${btoa(contents)}`;
-      $SD.setImage(this.context, encoded, 1);
-    }
-  }
-
-  stop() {
-    console.log("Stopping stopwatch");
-    this.stopTick();
-    $SD.clearTitle(this.context);
-    this.state = States.Stopped;
-  }
-
-  pause() {
-    console.log("Pausing stopwatch");
-    this.stopTick();
-    this.state = States.Paused;
-    this.add = new Date() - this.startTime;
-  }
-
-  unpause() {
-    console.log("Unpausing stopwatch");
-    this.state = States.Running;
-    this.startTime = new Date() - this.add;
-    this.beginTick();
-  }
-
-  keyDown({ payload: { state } }) {
-    this.cancelKeyUp = false;
-    this.pressTimeout = setTimeout(() => {
-      if (this.state !== States.Stopped) {
-        this.cancelKeyUp = true;
-        if (this.settings.longPressAction === Actions.Reset) {
-          this.stop();
-        } else if (this.settings.longPressAction === Actions.Pause) {
-          if (this.state === States.Running) {
-            this.pause();
-          } else if (this.state === States.Paused) {
-            this.unpause();
-          }
-        }
-        $SD.setState(this.context, this.sdState);
-      }
-    }, this.settings.longPressTime);
-  }
-
-  configureFrame(render = true) {
-    if (this.frame) {
-      this.frame.querySelector("path").style.fill = this.settings.frameColor;
-      this.frame.querySelector("text").style.fill = this.settings.textColor;
-      if (render) {
-        this.tick();
-      }
-    }
-  }
-
   set settings(settings) {
     if (Object.keys(settings).length === 0) {
       return;
@@ -109,6 +31,98 @@ class Stopwatch {
       return 0;
     }
     return 1;
+  }
+
+  get formattedTime() {
+    let seconds = Math.floor((new Date() - this.startTime) / 1000);
+    let minutes = Math.floor(seconds / 60);
+
+    if (minutes >= 60) {
+      const hours = Math.floor(minutes / 60);
+      minutes %= 60;
+      return `${hours}:${minutes.toString().padStart(2, "0")}`;
+    } else {
+      seconds %= 60;
+      return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+    }
+  }
+
+  tick() {
+    if (this.frame) {
+      this.frame.querySelector("text").innerHTML = this.formattedTime;
+      const contents = new XMLSerializer().serializeToString(this.frame);
+      const encoded = `data:image/svg+xml;base64,${btoa(contents)}`;
+      $SD.setImage(this.context, encoded, 1);
+    }
+  }
+
+  beginTick() {
+    this.tick();
+    this.stopTick();
+    this.tickInterval = setInterval(() => this.tick(), 1000);
+  }
+
+  stopTick() {
+    if (this.tickInterval !== null) {
+      clearInterval(this.tickInterval);
+    }
+  }
+
+  configureFrame(render = true) {
+    if (this.frame) {
+      this.frame.querySelector("path").style.fill = this.settings.frameColor;
+      this.frame.querySelector("text").style.fill = this.settings.textColor;
+      if (render) {
+        this.tick();
+      }
+    }
+  }
+
+  start() {
+    console.log("Starting stopwatch");
+    this.startTime = new Date();
+    this.beginTick();
+    this.state = States.Running;
+  }
+
+  stop() {
+    console.log("Stopping stopwatch");
+    this.stopTick();
+    $SD.clearTitle(this.context);
+    this.state = States.Stopped;
+  }
+
+  pause() {
+    console.log("Pausing stopwatch");
+    this.stopTick();
+    this.add = new Date() - this.startTime;
+    this.state = States.Paused;
+  }
+
+  unpause() {
+    console.log("Unpausing stopwatch");
+    this.startTime = new Date() - this.add;
+    this.beginTick();
+    this.state = States.Running;
+  }
+
+  keyDown({ payload: { state } }) {
+    this.cancelKeyUp = false;
+    this.pressTimeout = setTimeout(() => {
+      if (this.state !== States.Stopped) {
+        this.cancelKeyUp = true;
+        if (this.settings.longPressAction === Actions.Reset) {
+          this.stop();
+        } else if (this.settings.longPressAction === Actions.Pause) {
+          if (this.state === States.Running) {
+            this.pause();
+          } else if (this.state === States.Paused) {
+            this.unpause();
+          }
+        }
+        $SD.setState(this.context, this.sdState);
+      }
+    }, this.settings.longPressTime);
   }
 
   keyUp() {
@@ -135,19 +149,5 @@ class Stopwatch {
     }
 
     $SD.setState(this.context, this.sdState);
-  }
-
-  get formattedTime() {
-    let seconds = Math.floor((new Date() - this.startTime) / 1000);
-    let minutes = Math.floor(seconds / 60);
-
-    if (minutes >= 60) {
-      const hours = Math.floor(minutes / 60);
-      minutes %= 60;
-      return `${hours}:${minutes.toString().padStart(2, "0")}`;
-    } else {
-      seconds %= 60;
-      return `${minutes}:${seconds.toString().padStart(2, "0")}`;
-    }
   }
 }
